79a2805cebf5f6010a415c06e54cc24d04eea672
AAKASH SINGH <saakashkumar@gmail.com>
2024-08-04 23:31:43 +0530

    Adding new Directry

---
+++ src/test/java/cucumberOptions/TestNGRunner.java
@@ -0,0 +1,15 @@
+package cucumberOptions;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions( features = "src/test/java/features", // Path to feature files
+        glue = "stepDefinition", // Path to step definitions
+        monochrome = true// Clean console output
+       // plugin = {
+              //  "pretty", // Pretty console output
+              //  "html:target/cucumber-reports"} // HTML report
+         )
+
+public class TestNGRunner extends AbstractTestNGCucumberTests {
+}
